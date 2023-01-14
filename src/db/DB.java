4773ca8d8fb97b49b2236c38060b789d3e74f720
Thierry Marinho <thierrydev48@gmail.com>
2023-01-14 15:37:46 -0300

    retrieve data from database

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,14 +2,13 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
 
     private static Connection conn;
+
     public static Connection getConnection() {
         if (conn == null) {
             try {
@@ -31,6 +30,23 @@ public class DB {
             throw new DbException(e.getMessage());
         }
     }
+
+    public static void closeStatement(Statement st) {
+        try {
+            st.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
     private static Properties loadProperties() {
         try (FileInputStream fs = new FileInputStream("db.properties")) {
             Properties props = new Properties();
