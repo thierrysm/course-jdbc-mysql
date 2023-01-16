3b69a7e3d8aaac0a20e0b016175b0eed0e630eac
Thierry Marinho <thierrydev48@gmail.com>
2023-01-16 08:53:18 -0300

    Updating data

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,49 +3,30 @@ package application;
 import db.DB;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "INSERT INTO seller"
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                    + "VALUES"
-                    + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS
-            );
 
-            st.setString(1, "Thierry");
-            st.setString(2, "thierry@gmail.com");
-            st.setDate(3, new Date(sdf.parse("27/07/1998").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
+
+            st.setDouble(1, 200);
+            st.setInt(2, 2);
 
             int rowsAffected = st.executeUpdate();
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done!  Id: " + id);
-                }
-            }
-            else {
-                System.out.println("None rows affected");
-            }
+
+            System.out.println("Done! rows affected " + rowsAffected);
         }
         catch (SQLException e) {
-            e.printStackTrace();
-        }
-        catch (ParseException e) {
             e.printStackTrace();
         }
         finally {
